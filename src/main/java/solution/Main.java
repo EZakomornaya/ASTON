9016a74b41b31e7d71c319ff0623a45c6ef526b3
EZakomornaya <e_naumchik@mail.ru>
2024-04-19 16:49:25 -0700

    init commit

---
+++ src/main/java/solution/Main.java
@@ -0,0 +1,12 @@
+package solution;
+
+public class Main {
+    public static void main(String[] args) {
+        printThreeWords("Orange");
+        printThreeWords("Banana");
+        printThreeWords("Apple");
+    }
+    public static void printThreeWords(String s) {
+        System.out.println(s);
+    }
+}
