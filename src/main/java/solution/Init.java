be75a11c4ea6251259f33b337fc9aad0b8b64f42
EZakomornaya <e_naumchik@mail.ru>
2024-04-20 16:42:51 -0700

    init commit

--- src/main/java/solution/Init.java
+++ src/main/java/solution/Init.java
@@ -1,12 +1,18 @@
 package solution;
 
+import java.util.Scanner;
+
 public class Init {
     public static void main(String[] args) {
-        int initialValue = 3;
-        int len = 10;
-        int mas[] = new int[len];
+        Scanner initialValue = new Scanner(System.in);
+        System.out.println("введите значение initialValue");
+        int x=initialValue.nextInt();
+        Scanner len = new Scanner(System.in);
+        System.out.println("введите значение len");
+        int y=len.nextInt();
+        int mas[] = new int[y];
         for (int i = 0; i < mas.length; ++i) {
-            mas[i] = initialValue;
+            mas[i] = x;
             System.out.print(mas[i] + " ");
         }
     }
