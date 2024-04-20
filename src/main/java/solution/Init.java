45b5a62d2073f6d75923683e35d95922dbb012ba
EZakomornaya <e_naumchik@mail.ru>
2024-04-20 15:55:47 -0700

    init commit

---
+++ src/main/java/solution/Init.java
@@ -0,0 +1,14 @@
+package solution;
+
+public class Init {
+    public static void main(String[] args) {
+        int initialValue = 3;
+        int len = 10;
+        int mas[] = new int[len];
+        for (int i = 0; i < mas.length; ++i) {
+            mas[i] = initialValue;
+            System.out.print(mas[i] + " ");
+        }
+    }
+    }
+
