fe3328e15209234399035dd704855d66d9835ce4
EZakomornaya <e_naumchik@mail.ru>
2024-04-20 12:56:08 -0700

    init commit

---
+++ src/main/java/solution/Stroka.java
@@ -0,0 +1,11 @@
+package solution;
+
+public class Stroka {
+    public static void main(String[] args) {
+        String test = "Black";
+        int i=8;
+        int x;
+        for (x=0; x<i; x++) System.out.println(test);
+    }
+
+}
