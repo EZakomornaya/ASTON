70be30a249ff7b5626dd911ee5e97eb965676f83
EZakomornaya <e_naumchik@mail.ru>
2024-04-20 16:54:52 -0700

    init commit

--- src/main/java/solution/Massivq.java
+++ src/main/java/solution/Massivq.java
@@ -2,16 +2,14 @@ package solution;
 
 public class Massivq {
     public static void main(String args[]) {
-        int mas[][] = new int[10][10];
+        int mas[][] = new int[5][5];
         for (int i = 0; i < mas.length; i++) {
-            for (int j = 0; j < mas.length; j++)
-                if (i == j)
-                    mas[i][j] = 1;
-        }
-        for (int i = 0; i < mas.length; i++) {
-            for (int j = 0; j < mas.length; j++)
+            for (int j = 0, x = mas[i].length - 1; j < mas[i].length; j++, x--) {
+                if (i == j || i == x) mas[i][j] = 1;
+                else mas[i][j] = 0;
                 System.out.print(mas[i][j] + " ");
-            System.out.println();
+            }
+                System.out.print("\n");
+    }
         }
     }
-}
