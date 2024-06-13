e6d29a6c69446bfb19563730e4f5ff6c0c8598a4
EZakomornaya <e_naumchik@mail.ru>
2024-06-12 22:13:24 -0700

    init commit

--- src/main/java/lesson_15/CheckMtsTest.java
+++ src/main/java/lesson_15/CheckMtsTest.java
@@ -4,7 +4,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -12,6 +14,7 @@ import java.util.List;
 
 public class CheckMtsTest {
     static WebDriver driver = null;
+    static WebDriverWait wait = null;
 
     @BeforeClass
     public static void setUp() {
@@ -20,12 +23,13 @@ public class CheckMtsTest {
         driver.get("http://mts.by");
         WebElement cookies = driver.findElement(By.xpath("//button[@id='cookie-agree']"));
         cookies.click();
+        wait = new WebDriverWait(driver,10);
     }
 
     //Проверка названия блока
     @Test
-    public void testCheckNameBlock() throws InterruptedException {
-        Thread.sleep(10000);
+    public void testCheckNameBlock() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='pay__wrapper']/h2")));
         WebElement blockTitle = driver.findElement(By.xpath("//div[@class='pay__wrapper']/h2"));
         String expectedTitle = "Онлайн пополнение\nбез комиссии";
         Assert.assertEquals(blockTitle.getText(), expectedTitle);
@@ -34,15 +38,15 @@ public class CheckMtsTest {
     //Проверка наличия логотипов платежных систем
     @Test
     public void testCheckIsLogo() throws InterruptedException {
-        Thread.sleep(5000);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//img[@alt='МТС']")));
         List<WebElement> logos = driver.findElements(By.xpath("//img[@alt='МТС']"));
         Assert.assertFalse(logos.isEmpty());
     }
 
     //Проверка работы ссылки Подробнее о сервисе
     @Test
-    public void testCheckWorkLink() throws InterruptedException {
-        Thread.sleep(5000);
+    public void testCheckWorkLink() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='pay__wrapper']/a")));
         WebElement moreInfoLink = driver.findElement(By.xpath("//div[@class='pay__wrapper']/a"));
         moreInfoLink.click();
         Assert.assertTrue(driver.getCurrentUrl().contains("https://www.mts.by/help/poryadok-oplaty-i-bezopasnost-internet-platezhey/"));
@@ -50,8 +54,8 @@ public class CheckMtsTest {
 
     //Заполнение полей и проверка работы кнопки Продолжить
     @Test
-    public void testCheckWorkFieldAndContinue() throws InterruptedException {
-        Thread.sleep(5000);
+    public void testCheckWorkFieldAndContinue() {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@id='connection-phone']")));
         WebElement phoneNumberField = driver.findElement(By.xpath("//input[@id='connection-phone']"));
         WebElement serviceDropDown = driver.findElement(By.xpath("//select[@id='pay']"));
         WebElement continueButton = driver.findElement(By.xpath("//button[@class='button button__default ']"));
