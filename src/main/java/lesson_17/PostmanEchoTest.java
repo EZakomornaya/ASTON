10d810b06963cae98b47ac65019fabb1cbf380d2
EZakomornaya <e_naumchik@mail.ru>
2024-06-22 18:31:56 -0700

    init commit

--- src/main/java/lesson_17/PostmanEchoTest.java
+++ src/main/java/lesson_17/PostmanEchoTest.java
@@ -17,17 +17,15 @@ public class PostmanEchoTest {
                         "keep-alive", "Accept", "*/*", "Cookie",
                         "sails.sid=s%3Aw_GiOdL4riO4COkx3XqK6D7ZLv3iW2ax.XzKjQlYVzm5Npo0MfU7c2wgMgHLkM%2FkQTX6TAZx32i0")
                 .queryParam("foo1", "bar1")
-                .queryParam("foo2", "bar2")
-
-                .when()
+                .queryParam("foo2", "bar2").
+        when()
                 .contentType(ContentType.JSON)
-                .get("https://postman-echo.com/get")
-
-                .then().log().all()
+                .get("https://postman-echo.com/get").
+        then()
+                .log().all()
                 .assertThat().statusCode(200)
                 .body("args.foo1", equalTo("bar1"))
                 .body("args.foo2", is("bar2"))
-
                 .body("headers.host", equalTo("postman-echo.com"))
                 .body("headers.x-request-start", notNullValue())
                 .body("headers.x-forwarded-proto", equalTo("https"))
@@ -37,10 +35,7 @@ public class PostmanEchoTest {
                 .body("headers.accept", equalTo("*/*"))
                 .body("headers.user-agent", equalTo("PostmanRuntime/7.39.0"))
                 .body("headers.accept-encoding", equalTo("gzip, deflate, br"))
-                .body("url", equalTo("https://postman-echo.com/get?foo1=bar1&foo2=bar2"))
-
-                .extract()
-                .response();
+                .body("url", equalTo("https://postman-echo.com/get?foo1=bar1&foo2=bar2"));
     }
 
     //2
@@ -52,15 +47,13 @@ public class PostmanEchoTest {
                         "User-Agent", "PostmanRuntime/7.39.0", "Connection",
                         "keep-alive", "Accept", "*/*", "Cookie",
                         "sails.sid=s%3AvwQx2fqMLErmjTBwFdO838LKXK_f68y0.gsyuf9NvpDiJhUOgQ6a3yhYU3JmOdvzrJmDfVxmFR5k")
-                .body("This is expected to be sent back as part of response body.")
-
-                .when()
-                .post("https://postman-echo.com/post")
-
-                .then().log().all()
+                .body("This is expected to be sent back as part of response body.").
+        when()
+                .post("https://postman-echo.com/post").
+        then()
+                .log().all()
                 .assertThat().statusCode(200)
                 .body("data", equalTo("This is expected to be sent back as part of response body."))
-
                 .body("headers.host", equalTo("postman-echo.com"))
                 .body("headers.x-request-start", notNullValue())
                 .body("headers.x-forwarded-proto", equalTo("https"))
@@ -71,10 +64,7 @@ public class PostmanEchoTest {
                 .body("headers.user-agent", equalTo("PostmanRuntime/7.39.0"))
                 .body("headers.accept-encoding", equalTo("gzip, deflate, br"))
                 .body("json", nullValue())
-                .body("url", equalTo("https://postman-echo.com/post"))
-
-                .extract()
-                .response();
+                .body("url", equalTo("https://postman-echo.com/post"));
     }
 
     //3
@@ -87,16 +77,14 @@ public class PostmanEchoTest {
                         "User-Agent", "PostmanRuntime/7.39.0", "Connection",
                         "keep-alive", "Accept", "*/*")
                 .formParam("foo1", "bar1")
-                .formParam("foo2", "bar2")
-
-                .when()
-                .post("https://postman-echo.com/post")
-
-                .then().log().all()
+                .formParam("foo2", "bar2").
+        when()
+                .post("https://postman-echo.com/post").
+        then()
+                .log().all()
                 .assertThat().statusCode(200)
                 .body("form.foo1", equalTo("bar1"))
                 .body("form.foo2", equalTo("bar2"))
-
                 .body("headers.host", equalTo("postman-echo.com"))
                 .body("headers.x-request-start", notNullValue())
                 .body("headers.x-forwarded-proto", equalTo("https"))
@@ -107,10 +95,7 @@ public class PostmanEchoTest {
                 .body("headers.user-agent", equalTo("PostmanRuntime/7.39.0"))
                 .body("headers.accept-encoding", equalTo("gzip, deflate, br"))
                 .body("json", notNullValue())
-                .body("url", equalTo("https://postman-echo.com/post"))
-
-                .extract()
-                .response();
+                .body("url", equalTo("https://postman-echo.com/post"));
     }
 
     //4
@@ -122,15 +107,13 @@ public class PostmanEchoTest {
                 .headers("Content-Type", "text/plain", "Accept-Encoding", "gzip, deflate, br",
                         "User-Agent", "PostmanRuntime/7.39.0", "Connection",
                         "keep-alive", "Accept", "*/*", "Cookie",
-                        "sails.sid=s%3A7B8FFYB6tKHWWCbGu0rYlwUXmyPTv91n.D9vvYh5imdsxSyPJrWRS07Es0FU8XZGymeI5O6hK6V0")
-
-                .when()
-                .put("https://postman-echo.com/put")
-
-                .then().log().all()
+                        "sails.sid=s%3A7B8FFYB6tKHWWCbGu0rYlwUXmyPTv91n.D9vvYh5imdsxSyPJrWRS07Es0FU8XZGymeI5O6hK6V0").
+        when()
+                .put("https://postman-echo.com/put").
+        then()
+                .log().all()
                 .assertThat().statusCode(200)
                 .body("data", equalTo("This is expected to be sent back as part of response body."))
-
                 .body("headers.host", equalTo("postman-echo.com"))
                 .body("headers.x-request-start", notNullValue())
                 .body("headers.x-forwarded-proto", equalTo("https"))
@@ -141,10 +124,7 @@ public class PostmanEchoTest {
                 .body("headers.user-agent", equalTo("PostmanRuntime/7.39.0"))
                 .body("headers.accept-encoding", equalTo("gzip, deflate, br"))
                 .body("json", nullValue())
-                .body("url", equalTo("https://postman-echo.com/put"))
-
-                .extract()
-                .response();
+                .body("url", equalTo("https://postman-echo.com/put"));
     }
 
     //5
@@ -156,15 +136,13 @@ public class PostmanEchoTest {
                 .headers("Content-Type", "text/plain", "Accept-Encoding", "gzip, deflate, br",
                         "User-Agent", "PostmanRuntime/7.39.0", "Connection",
                         "keep-alive", "Accept", "*/*", "Cookie",
-                        "sails.sid=s%3AELeBrvjbuXA2H1rsCF4JMuyR8R9fabPG.wn27i3QuMJUWgOCH8T5qcSKczt50BCnhwM4f0sI96iY")
-
-                .when()
-                .patch("https://postman-echo.com/patch")
-
-                .then().log().all()
+                        "sails.sid=s%3AELeBrvjbuXA2H1rsCF4JMuyR8R9fabPG.wn27i3QuMJUWgOCH8T5qcSKczt50BCnhwM4f0sI96iY").
+        when()
+                .patch("https://postman-echo.com/patch").
+        then()
+                .log().all()
                 .assertThat().statusCode(200)
                 .body("data", equalTo("This is expected to be sent back as part of response body."))
-
                 .body("headers.host", equalTo("postman-echo.com"))
                 .body("headers.x-request-start", notNullValue())
                 .body("headers.x-forwarded-proto", equalTo("https"))
@@ -175,10 +153,7 @@ public class PostmanEchoTest {
                 .body("headers.user-agent", equalTo("PostmanRuntime/7.39.0"))
                 .body("headers.accept-encoding", equalTo("gzip, deflate, br"))
                 .body("json", nullValue())
-                .body("url", equalTo("https://postman-echo.com/patch"))
-
-                .extract()
-                .response();
+                .body("url", equalTo("https://postman-echo.com/patch"));
     }
 
     //6
@@ -190,15 +165,13 @@ public class PostmanEchoTest {
                 .headers("Content-Type", "text/plain", "Accept-Encoding", "gzip, deflate, br",
                         "User-Agent", "PostmanRuntime/7.39.0", "Connection",
                         "keep-alive", "Accept", "*/*", "Cookie",
-                        "sails.sid=s%3Ax8b_uK-WUQtiAZN_dPSd8genr4sSNFPS.E%2FwE9FKuPbToIodSM%2FWtV%2BfvjaHNZAs%2FmEa6ZV8MVYE")
-
-                .when()
-                .delete("https://postman-echo.com/delete")
-
-                .then().log().all()
+                        "sails.sid=s%3Ax8b_uK-WUQtiAZN_dPSd8genr4sSNFPS.E%2FwE9FKuPbToIodSM%2FWtV%2BfvjaHNZAs%2FmEa6ZV8MVYE").
+        when()
+                .delete("https://postman-echo.com/delete").
+        then()
+                .log().all()
                 .assertThat().statusCode(200)
                 .body("data", equalTo("This is expected to be sent back as part of response body."))
-
                 .body("headers.host", equalTo("postman-echo.com"))
                 .body("headers.x-request-start", notNullValue())
                 .body("headers.x-forwarded-proto", equalTo("https"))
@@ -209,10 +182,7 @@ public class PostmanEchoTest {
                 .body("cache-control", nullValue())
                 .body("headers.accept-encoding", equalTo("gzip, deflate, br"))
                 .body("json", nullValue())
-                .body("url", equalTo("https://postman-echo.com/delete"))
-
-                .extract()
-                .response();
+                .body("url", equalTo("https://postman-echo.com/delete"));
     }
 }
 
